d040bda85b9813240590446268825371e940d78e
Sergiusz Kriwobokow <writesd@hotmail.com>
2021-05-17 23:17:59 +0200

    Add project files.

---
+++ BlazorHostedRolebasedSecurity/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using BlazorHostedRolebasedSecurity.Server.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorHostedRolebasedSecurity.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new RoleConfiguration());
+        }
+    }
+}
